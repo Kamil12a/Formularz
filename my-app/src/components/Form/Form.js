6975d41a03ba2fdcc30210a34330dbcfd1d7899b
kamil plewka <kamilplewka12a@wp.pl>
2022-09-12 17:46:06 +0200

    add styled-components, create structure of a project

---
+++ my-app/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import { MainContainer,SForm,WelcomeText } from "./styles";
+const Form = () => {
+  return (
+    <MainContainer>
+      <SForm>
+        <WelcomeText>Napisz do nas</WelcomeText>
+
+      </SForm>
+    </MainContainer>
+  );
+};
+
+export default Form;
---
+++ my-app/src/components/Form/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { device } from "../../styles/variables";
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: black;
+`;
+export const SForm = styled.div`
+  background: white;
+  width: 60vh;
+  height: 80vh;
+  border: 2 px solid black;
+  background-color: #f9f9f9;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
+export const WelcomeText = styled.h2`
+  text-align: center;
+`;
